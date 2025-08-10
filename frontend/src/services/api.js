c6ea7be4451d4ddcc89561b1a71f8be710428add
Swarup Patil <swarupaa619yes@gmail.com>
2025-08-10 16:46:17 +0530

    api url added

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/apiService.js
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "https://whatsapp-web-clone-1vln.onrender.com/";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
